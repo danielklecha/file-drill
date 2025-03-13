028bc86d777fe59386f78638a133db215938b9dc
Daniel Klecha <daniel.klecha@gmail.com>
2025-03-13 12:27:42 +0100

    update installation script

---
+++ Setup/Install-Tool.ps1
@@ -0,0 +1,19 @@
+Push-Location
+Set-Location "$PSScriptRoot\.."
+
+try {
+    Write-Host "Starting the build process..." -ForegroundColor Cyan
+    dotnet build --configuration Release FileDrill.sln
+
+    Write-Host "Packing the tool..." -ForegroundColor Cyan
+    dotnet pack --no-build --configuration Release FileDrill.sln
+
+    Write-Host "Installing the tool globally..." -ForegroundColor Cyan
+    dotnet tool update --global --add-source .\FileDrill\bin\Release --no-cache FileDrill
+
+    Write-Host "Installation complete!" -ForegroundColor Green
+    Read-Host -Prompt "Press Enter to exit..."
+}
+finally {
+    Pop-Location
+}
